9b86fef1caab90824fde74a7836165033e2f4723
MahetsiEdahi <daniel.olvera.v1905@gmail.com>
2024-05-30 01:52:34 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name='mlproject',
+version='0.0.1',
+author= 'Mahetsi',
+author_email='daniel.olvera.v1905@gmail.com',
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn']    
+)
